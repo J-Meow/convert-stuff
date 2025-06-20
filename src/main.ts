c0d956b54ab4f92bd3b4b0a6671ef9e7114e6e95
Jmeow <j@jmeow.net>
2025-06-20 18:58:33 -0500

    Add dropping effect + remove default drop behavior

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import "./main.scss"
+document.documentElement.addEventListener("dragover", (event) => {
+    event.preventDefault()
+    event.dataTransfer!.dropEffect = "copy"
+    if (!document.querySelector("#droptarget")?.classList.contains("drophover")) {
+        document.querySelector("#droptarget")?.classList.add("drophover")
+        document.querySelector("#droptarget")!.innerHTML = "Drop it!"
+    }
+})
+document.documentElement.addEventListener("dragleave", (event) => {
+    event.preventDefault()
+    document.querySelector("#droptarget")?.classList.remove("drophover")
+    document.querySelector("#droptarget")!.innerHTML = "Drag your files here"
+})
+document.documentElement.addEventListener("drop", (event) => {
+    event.preventDefault()
+    document.querySelector("#droptarget")?.classList.remove("drophover")
+    document.querySelector("#droptarget")!.innerHTML = "Drag your files here"
+})
