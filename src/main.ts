871e5738ead3eed0b37103a8fa727384c1e98458
Jmeow <j@jmeow.net>
2025-06-20 19:24:22 -0500

    Add drop behavior

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
 import "./main.scss"
+function dropError(msg: string) {
+    document.querySelector("#droptarget")!.innerHTML = msg
+    document.querySelector("#droptarget")!.classList.add("error")
+    console.error(msg)
+    setTimeout(() => {
+        if (document.querySelector("#droptarget")?.classList.contains("error")) {
+            document.querySelector("#droptarget")?.classList.remove("error")
+            document.querySelector("#droptarget")!.innerHTML = "Drag your files here"
+        }
+    }, 1000)
+}
+function handleFile(file: File) {
+    console.log(file)
+    const extension = file.name.split(".").pop()!
+    if (!extension) {
+        dropError("Unsupported file type")
+    }
+    const mimeType = file.type
+    switch (mimeType + "_" + extension) {
+        case "image/png_png":
+            dropError("WIP")
+            break
+
+        default:
+            dropError("Unsupported file type")
+            break
+    }
+}
 document.documentElement.addEventListener("dragover", (event) => {
     event.preventDefault()
     event.dataTransfer!.dropEffect = "copy"
+    if (document.querySelector("#droptarget")?.classList.contains("error")) {
+        document.querySelector("#droptarget")?.classList.remove("error")
+    }
     if (!document.querySelector("#droptarget")?.classList.contains("drophover")) {
         document.querySelector("#droptarget")?.classList.add("drophover")
         document.querySelector("#droptarget")!.innerHTML = "Drop it!"
@@ -16,4 +47,7 @@ document.documentElement.addEventListener("drop", (event) => {
     event.preventDefault()
     document.querySelector("#droptarget")?.classList.remove("drophover")
     document.querySelector("#droptarget")!.innerHTML = "Drag your files here"
+    const file = event.dataTransfer?.files[0]
+    if (!file) return
+    handleFile(file)
 })
